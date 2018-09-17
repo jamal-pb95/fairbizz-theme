05d7b247b8bd776704224cad84e90f1176d01b6f
jamal-pb95 <jamal.pb95@gmail.com>
2018-09-17 13:56:39 +0600

    services

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function($) {
   "use strict";
 
-  // slider
+  // main slider
   function mainSlider() {
     var BasicSlider = $(".slider-active");
     BasicSlider.on("init", function(e, slick) {
@@ -21,8 +21,8 @@
       autoplaySpeed: 10000,
       dots: true,
       fade: true,
-        prevArrow: '<button type="button" class="slick-prev"> <i class="lnr lnr-arrow-left"></i> prev </button>',
-        nextArrow:'<button type="button" class="slick-next"><i class="lnr lnr-arrow-right"></i> Next</button>',
+      prevArrow: '<button type="button" class="slick-prev"> <i class="lnr lnr-arrow-left"></i> prev </button>',
+      nextArrow: '<button type="button" class="slick-next"><i class="lnr lnr-arrow-right"></i> Next</button>',
       arrows: true,
       responsive: [
         { breakpoint: 767, settings: { dots: false, arrows: false } }
@@ -47,4 +47,44 @@
     }
   }
   mainSlider();
+
+  // Service slider
+  $('.service-active').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
 })(jQuery);
